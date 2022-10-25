a4e51c996b4a8f6d258031ec5e5f33c6476e3499
Mika Rinne <mika.rinne@oracle.com>
2022-10-25 12:37:18 +0200

    create

--- script.sh
+++ script.sh
@@ -1,8 +1,6 @@
 export region='eu-amsterdam-1'
 export compt_ocid='<YOUR COMPARTMENT OCID>'
-oci db autonomous-database create --compartment-id  $compt_ocid --db-name pricing --cpu-core-count 1 --data-storage-size-in-tbs 1 --admin-password 'WelcomeFolks123#!' --display-name pricing
-sleep 20
-export ocid=$(oci db autonomous-database list --compartment-id $compt_ocid | jq -r '.data[] | select( ."db-name" == "pricing" ).id')
+export ocid=$(oci db autonomous-database create --compartment-id  $compt_ocid --db-name pricing --cpu-core-count 1 --data-storage-size-in-tbs 1 --admin-password 'WelcomeFolks123#!' --display-name pricing | jq -r '.data.id)
 echo "ATP: $ocid"
 export tries=0
 export atp_status=''
@@ -13,13 +11,16 @@ do
   tries=$(( $tries + 1 ))
   sleep 5
 done
+sh gen_apex.sh
 oci db autonomous-database generate-wallet --autonomous-database-id $ocid --password 'WelcomeFolks123#!' --file wallet.zip
 mkdir -p network/admin
 mv wallet.zip network/admin/
-unzip network/admin/
-sh gen_apex.sh
-export atp_link=$(grep -oP '(?<=service_name=)[^_]*' ./network/admin/tnsnames.ora | echo "https://$(head -n 1)-pricing.adb.${region}.oraclecloudapps.com/ords/r/priceadmin/price-admin/login")
+cd network/admin
+unzip wallet.zip
+cd ..
+export url=$(grep -oP '(?<=service_name=)[^_]*' ./network/admin/tnsnames.ora | echo "https://$(head -n 1)-pricing.adb.${region}.oraclecloudapps.com/ords/r/priceadmin/price-admin/login")
 sed -i "s/URL/$url/g" vue.js
 oci os bucket create --compartment-id  $compt_ocid --name pricing --public-access-type ObjectReadWithoutList
-oci os object bulk-delete --bucket-name pricing --force
-oci os object bulk-upload --bucket-name pricing --overwrite --src-dir . --content-type "text/html"
+oci os object put --force --bucket-name pricing --file index.html --content-type "text/html" --force
+oci os object put --force --bucket-name pricing --file vue.js --content-type "text/javascript" --force
+oci os object put --force --bucket-name pricing --file pricing.css  --content-type "text/css" --force
