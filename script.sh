b7cb0e1a6cd7e41aaefcc16f0b58a0ae4297fd4c
Mika Rinne <mika.rinne@oracle.com>
2022-10-25 12:41:35 +0200

    create

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 export region='eu-amsterdam-1'
 export compt_ocid='<YOUR COMPARTMENT OCID>'
-export ocid=$(oci db autonomous-database create --compartment-id  $compt_ocid --db-name pricing --cpu-core-count 1 --data-storage-size-in-tbs 1 --admin-password 'WelcomeFolks123#!' --display-name pricing | jq -r '.data.id)
+export ocid=$(oci db autonomous-database create --compartment-id  $compt_ocid --db-name pricing --cpu-core-count 1 --data-storage-size-in-tbs 1 --admin-password 'WelcomeFolks123#!' --display-name pricing | jq -r '.data.id')
 echo "ATP: $ocid"
 export tries=0
 export atp_status=''
@@ -17,7 +17,7 @@ mkdir -p network/admin
 mv wallet.zip network/admin/
 cd network/admin
 unzip wallet.zip
-cd ..
+cd ../..
 export url=$(grep -oP '(?<=service_name=)[^_]*' ./network/admin/tnsnames.ora | echo "https://$(head -n 1)-pricing.adb.${region}.oraclecloudapps.com/ords/r/priceadmin/price-admin/login")
 sed -i "s/URL/$url/g" vue.js
 oci os bucket create --compartment-id  $compt_ocid --name pricing --public-access-type ObjectReadWithoutList
