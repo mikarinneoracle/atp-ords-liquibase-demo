3c189d2a8adfdb0f0a40a4f9c430a3772c565a6d
Mika Rinne <mika.rinne@oracle.com>
2022-10-26 14:10:19 +0200

    tf added

---
+++ terraform/atp.tf
@@ -0,0 +1,12 @@
+resource "oci_database_autonomous_database" "pricing_autonomous_database" {
+  
+  #Required
+  admin_password           = "WelcomeFolks123#!"
+  compartment_id           = var.compartment_ocid
+  cpu_core_count           = 1
+  data_storage_size_in_tbs = 1
+  db_name                  = "pricing"
+  is_free_tier             = var.use_always_free
+  db_workload              = "OLTP"
+  display_name             = "pricing"
+}
