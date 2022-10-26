31fdb3d5e31732e7145d1baaeb5cd1e9c0e2505f
Mika Rinne <mika.rinne@oracle.com>
2022-10-26 18:17:47 +0200

    tf files and script

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -6,7 +6,7 @@ resource "oci_objectstorage_bucket" "pricing" {
 }
 
 resource "oci_objectstorage_object" "index_html" {
-  bucket    = oci_objectstorage_bucket.phonebook_public.name
+  bucket    = oci_objectstorage_bucket.pricing.name
   content   = file("./html/index.html")
   namespace = data.oci_objectstorage_namespace.user_namespace.namespace
   object    = "index.html"
@@ -14,7 +14,7 @@ resource "oci_objectstorage_object" "index_html" {
 }
 
 resource "oci_objectstorage_object" "vue_js" {
-  bucket    = oci_objectstorage_bucket.phonebook_public.name
+  bucket    = oci_objectstorage_bucket.pricing.name
   content   = file("./html/vue.js")
   namespace = data.oci_objectstorage_namespace.user_namespace.namespace
   object    = "vue.js"
@@ -22,7 +22,7 @@ resource "oci_objectstorage_object" "vue_js" {
 }
 
 resource "oci_objectstorage_object" "pricing_css" {
-  bucket    = oci_objectstorage_bucket.phonebook_public.name
+  bucket    = oci_objectstorage_bucket.pricing.name
   content   = file("./html/pricing.css")
   namespace = data.oci_objectstorage_namespace.user_namespace.namespace
   object    = "pricing.css"
