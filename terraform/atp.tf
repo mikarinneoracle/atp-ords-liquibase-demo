17e25b15ef1c53aacbdc0c0c565f7aaa95dd0b4f
Mika Rinne <mika.rinne@oracle.com>
2022-10-27 08:05:05 +0200

    tf files and script

--- terraform/atp.tf
+++ terraform/atp.tf
@@ -1,6 +1,4 @@
 resource "oci_database_autonomous_database" "pricing_autonomous_database" {
-  
-  #Required
   admin_password           = "WelcomeFolks123#!"
   compartment_id           = var.compartment_ocid
   cpu_core_count           = 1
@@ -9,6 +7,15 @@ resource "oci_database_autonomous_database" "pricing_autonomous_database" {
   is_free_tier             = var.use_always_free
   db_workload              = "OLTP"
   display_name             = "pricing"
+}
+
+resource "oci_database_autonomous_database_wallet" "autonomous_database_wallet" {
+  autonomous_database_id = oci_database_autonomous_database.pricing_autonomous_database.id
+  password               = "WelcomeFolks123#!"
+  base64_encode_content  = "true"
+  provisioner "local-exec" {
+    command = "ls"
+  }
 }
 
 output "atp" {
