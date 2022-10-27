3fff5a8397cffded5b23d2e0637c3b220017359d
Mika Rinne <mika.rinne@oracle.com>
2022-10-27 17:53:06 +0200

    quick fix for sqlcli to use v. 4.9.1

--- script.sh
+++ script.sh
@@ -20,7 +20,7 @@ tar -xzf jdk-11.0.16_linux-x64_bin.tar.gz
 export PATH=./jdk-11.0.16/bin:$PATH
 export JAVA_HOME=./jdk-11.0.16
 wget https://objectstorage.eu-amsterdam-1.oraclecloud.com/p/rBz7NIZQsEXsXN6yqnLn8m9fLmTHZUY7Z5uhPBBUzsoiW0ceUoY1jyU5y7DjEWJx/n/frsxwtjslf35/b/oracledb/o/V1022102-01.zip -q
-unzip V1022102-01.zip
+unzip -q V1022102-01.zip
 ./sqlcl/bin/sql -v
 echo "--------------------------------------------------------------------------------------------------------"
 sh gen_apex.sh
