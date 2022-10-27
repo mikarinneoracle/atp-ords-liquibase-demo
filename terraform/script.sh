e5b71c108a10f4573750086ba4c10a83b6d7a2df
Mika Rinne <mika.rinne@oracle.com>
2022-10-27 10:22:05 +0200

    tf files and script

--- terraform/script.sh
+++ terraform/script.sh
@@ -6,7 +6,10 @@ mv wallet.zip ./network/admin/
 cd ./network/admin
 unzip -q wallet.zip
 cd ../..
+ls -R
 export url=$(grep -oP '(?<=service_name=)[^_]*' ./network/admin/tnsnames.ora | echo "https://$(head -n 1)-pricing.adb.${region}.oraclecloudapps.com/ords/priceadmin")
 export apex=$(grep -oP '(?<=service_name=)[^_]*' ./network/admin/tnsnames.ora | echo "https://$(head -n 1)-pricing.adb.${region}.oraclecloudapps.com/ords/r/priceadmin/price-admin/login")
 echo $url
 echo $apex
+sed -i "s|URL|$url|g" ./html/vue.js
+sed -i "s|URL|$apex|g" ./html/index.html
