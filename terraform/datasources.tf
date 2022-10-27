34bdadb1d638cd0addabb5ce5d8157fd088bbea8
Mika Rinne <mika.rinne@oracle.com>
2022-10-27 10:57:51 +0200

    tf files and script

--- terraform/datasources.tf
+++ terraform/datasources.tf
@@ -1,3 +1,17 @@
 data "oci_objectstorage_namespace" "user_namespace" {
   compartment_id = var.compartment_ocid
 }
+
+data "template_file" "index" {
+  template = file("./html/index.html")
+  vars = {
+    URL = var.apex
+  }
+}
+
+data "template_file" "vue" {
+  template = file("./html/vue.js")
+  vars = {
+    URL = var.url
+  }
+}
