3efa4242d0ccced970594a95002cc14e8352c722
Mika Rinne <mika.rinne@oracle.com>
2022-10-28 14:26:01 +0200

    Terraform testing

--- terraform/datasources.tf
+++ terraform/datasources.tf
@@ -2,8 +2,8 @@ data "oci_objectstorage_namespace" "user_namespace" {
   compartment_id = var.compartment_ocid
 }
 
-data "template_file" "index" {
-  template = "${file("./html/index.html")}"
+data "template_file" "vue" {
+  template = "${file("./html/vue.js")}"
   vars = {
     APEX_URL = "${var.apex_url}"
     ORDS_URL = "${var.ords_url}"
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -7,7 +7,7 @@ resource "oci_objectstorage_bucket" "pricing" {
 
 resource "oci_objectstorage_object" "index_html" {
   bucket    = oci_objectstorage_bucket.pricing.name
-  content   = data.template_file.index.rendered
+  content   = file("./html/index.html")
   namespace = data.oci_objectstorage_namespace.user_namespace.namespace
   object    = "index.html"
   content_type = "text/html"
@@ -16,7 +16,7 @@ resource "oci_objectstorage_object" "index_html" {
 
 resource "oci_objectstorage_object" "vue_js" {
   bucket    = oci_objectstorage_bucket.pricing.name
-  content   = file("./html/vue.js")
+  content   = data.template_file.vue.rendered
   namespace = data.oci_objectstorage_namespace.user_namespace.namespace
   object    = "vue.js"
   content_type = "text/javascript"
