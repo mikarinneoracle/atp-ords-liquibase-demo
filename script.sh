cadcea0ca57153498461e8fda44d6003d6a7131e
Mika Rinne <mika.rinne@oracle.com>
2022-10-28 09:46:34 +0200

    Liquibase script change

--- script.sh
+++ script.sh
@@ -22,7 +22,7 @@ export JAVA_HOME=./jdk-11.0.16
 wget https://objectstorage.eu-amsterdam-1.oraclecloud.com/p/rBz7NIZQsEXsXN6yqnLn8m9fLmTHZUY7Z5uhPBBUzsoiW0ceUoY1jyU5y7DjEWJx/n/frsxwtjslf35/b/oracledb/o/V1022102-01.zip -q
 unzip -q V1022102-01.zip
 ./sqlcl/bin/sql -v
-echo "--------------------------------------------------------------------------------------------------------"
+echo "-------------------------------------------------------------------------------------------------------------------"
 sh gen_apex.sh
 cd ./network/admin
 unzip -q wallet.zip
