2858919f4378c9b227c9c6a106c2a482656d788d
Mika Rinne <mika.rinne@oracle.com>
2023-09-25 14:45:58 +0300

    tf output improved

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,3 @@
 output "application_url_in_bucket" {
-  value = "http://objectstorage.${oci_objectstorage_object.index_html.source_uri_details.region}.oraclecloud.com/n/${oci_objectstorage_object.index_html.source_uri_details.namespace}/b/${oci_objectstorage_object.index_html.source_uri_details.bucket}/o/${oci_objectstorage_object.index_html.source_uri_details.object}"
+  value = "http://objectstorage.${oci_objectstorage_object.index_html.source_uri_details[0].region}.oraclecloud.com/n/${oci_objectstorage_object.index_html.source_uri_details[0].namespace}/b/${oci_objectstorage_object.index_html.source_uri_details[0].bucket}/o/${oci_objectstorage_object.index_html.source_uri_details[0].object}"
 }
